f02329b09b27ae92286597d91cf94b5f38b6c16f
Matheus-Hen <matheushlsilva2000@gmail.com>
2022-04-30 20:40:48 -0300

    Cifra de Cesar finalizado, iniciado o codigo de codificacao de base64

--- criptografia.js
+++ criptografia.js
@@ -1,15 +1,79 @@
-Seleciona()
+document.getElementById('Seleciona').addEventListener("change", function () {
+    var Selec = document.getElementById('Seleciona')
+    if (Selec.options[0].selected) {
+        var Text1 = document.querySelector('#Texts')
+        var Verify = document.querySelector('#Incremento')
+        if (Verify) return Verify.remove()
+        var NewTextArea = document.createElement('input')
+        Text1.append(NewTextArea)
+        NewTextArea.id = "Incremento";
+        NewTextArea.type = 'text'
+        NewTextArea.placeholder = 'Insira o incremento'
+    }
+    else{
+        var Verify = document.querySelector('#Incremento')
+        if (Verify) return Verify.remove()
+    }
 
-function Seleciona(){
-    var Choose = document.getElementById('Seleciona')
-    if(Choose.value === 'CifraDeCesar') return Cifra()
-    else if(Choose.value === 'Base64') return Base64() 
 }
+)
+var cripto = []
+var Retorna = []
+var NewWorld
+var bin = []
+document.getElementById('Codificar').addEventListener('click', function () {
+    var Selec = document.getElementById('Seleciona')
+    if (Selec.options[0].selected) {
+        var Mensg = document.getElementById('Text1').value
+        var incre = parseInt(document.getElementById('Incremento').value)
+        var Mensagem = Mensg.split('')
+        if (Mensg != null) {
+            for (var i = 0; Mensagem.length > i; i++) {
+                cripto[i] = Mensagem[i].charCodeAt(0)
+                cripto[i] = cripto[i] + incre
+                Retorna[i] = String.fromCharCode(cripto[i])
+                console.log(Retorna[i])
+            }
+        }
 
-function Cifra(){
-    var Form = document.querySelector('#Formul')
-    var NewTextArea = document.createElement('input')
-    Form.append(NewTextArea)
-    NewTextArea.type = 'text'
-    NewTextArea.placeholder = 'Insira o incremento'
-}
+    }
+    else if (Selec.options[1].selected) {
+        var Mensg = document.getElementById('Text1').value
+        var Mensagem = Mensg.split('')
+        if (Mensg != null) {
+            for (var i = 0; Mensagem.length > i; i++) {
+                cripto[i] = Mensagem[i].charCodeAt(0)
+                bin[i] = cripto[i].toString(2) + cripto[i].toString(2) 
+                console.log(bin[i])
+            }
+        }
+    }
+})
+
+
+
+
+
+
+
+
+
+
+document.getElementById('Descodificar').addEventListener('click', function () {
+    var Selec = document.getElementById('Seleciona')
+    if ((Selec.options[0].selected)) {
+        var incre = parseInt(document.getElementById('Incremento').value)
+        var Mensagem = Mensg.split('')
+        console.log(incre);
+        if (Mensg != null) {
+            for (var i = 0; Mensagem.length > i; i++) {
+                cripto[i] = Mensagem[i].charCodeAt(0)
+                cripto[i] = cripto[i] - incre
+                Retorna[i] = String.fromCharCode(cripto[i])
+                console.log(Retorna[i])
+            }
+            NewWorld = Retorna.join('')
+            console.log(NewWorld)
+        }
+    }
+})
